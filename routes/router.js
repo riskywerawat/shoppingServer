e9fce5cc71cbc8a30a1877cd52c3f539b3e3d898
[Dev]weerawat.t <weerawat.t@pttdigital.com>
2021-02-25 17:12:08 +0700

    space bar

--- routes/router.js
+++ routes/router.js
@@ -17,6 +17,7 @@ exports.getuserFromId = (req, res) => {
     })
 };
 
+
 exports.getallProduct = (req, res) => {
     db.query("SELECT pr.id AS id , pr.name AS name , pr.price AS price , pr.fullPrice AS fullPrice , pr.productKey AS productKey, pr.stock AS stock, pr.img AS img, pr.status AS status, b.name AS brand, pt.name_th AS typeName FROM products AS pr INNER JOIN brand AS b ON pr.brand = b.id INNER JOIN product_type AS pt ON pr.type = pt.id WHERE pr.status = 'Y'", (err, result) => {
         if (err) {
